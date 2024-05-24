560488dc385a7bc4fdf16b5bdf04fc827ec250e5
Juan José Villada Calle <88852332+jvillada1@users.noreply.github.com>
2024-05-23 21:08:03 -0500

    Update pokeneas.py

--- pokeneas.py
+++ pokeneas.py
@@ -5,33 +5,28 @@ import socket
 
 app = Flask(__name__)
 
-# Cargar los datos de Pokeneas desde el archivo JSON
-with open('pokeneas.json', 'r') as f:
-    data = json.load(f)
-    pokeneas = data['pokeneas']
-
-def get_container_id():
-    # Obtener el nombre del host como un identificador del contenedor
-    return socket.gethostname()
+# Leer datos del archivo JSON
+with open('pokeneas.json') as f:
+    pokeneas = json.load(f)
 
 @app.route('/api/pokenea', methods=['GET'])
 def get_pokenea():
     pokenea = random.choice(pokeneas)
-    container_id = get_container_id()
+    container_id = socket.gethostname()
     response = {
-        "id": pokenea["id"],
-        "nombre": pokenea["nombre"],
-        "altura": pokenea["altura"],
-        "habilidad": pokenea["habilidad"],
-        "container_id": container_id
+        'id': pokenea['id'],
+        'nombre': pokenea['nombre'],
+        'altura': pokenea['altura'],
+        'habilidad': pokenea['habilidad'],
+        'container_id': container_id
     }
     return jsonify(response)
 
 @app.route('/pokenea', methods=['GET'])
 def show_pokenea():
     pokenea = random.choice(pokeneas)
-    container_id = get_container_id()
+    container_id = socket.gethostname()
     return render_template('pokenea.html', pokenea=pokenea, container_id=container_id)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
