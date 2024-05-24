b817d5080753c1781da0b2530d9b9fdc1848da77
Juan José Villada Calle <88852332+jvillada1@users.noreply.github.com>
2024-05-23 20:30:40 -0500

    Update pokeneas.py

--- pokeneas.py
+++ pokeneas.py
@@ -34,4 +34,4 @@ def show_pokenea():
     return render_template('pokenea.html', pokenea=pokenea, container_id=container_id)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
